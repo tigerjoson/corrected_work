49dd6a7a2bbeb5984791daf08064bd6974509cf4
tigerjoson <tigerjoson@gmail.com>
2018-08-24 16:06:07 +0800

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   file_RW.java

--- file_RW.java
+++ file_RW.java
@@ -9,14 +9,23 @@ public class  file_RW{
 	
 	public static void copy(String sorce_file,String destination_folder)throws  IOException {
 		try{
+			//Read
 			InputStream is = new FileInputStream(sorce_file);
 			InputStreamReader isr = new InputStreamReader(is);
 			BufferedReader bR = new BufferedReader(isr);
+			//write
+			OutputStream outputStream = new FileOutputStream(destination_folder);
+			OutputStreamWriter osr = new OutputStreamWriter(outputStream,"gbk");
+			BufferedWriter bWriter = new BufferedWriter(osr);
+			
 			
 			//close
 			bR.close();
 			isr.close();
 			is.close();
+			bWriter.close();
+			osr.close();
+			outputStream.close();
 			
 		}catch(FileNotFoundException e){
 			System.out.println("FileNotFoundException e");
